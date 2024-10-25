fbf5b3da1516c0dd63680f9b92358ebec2c65b47
Aniket Dutta <101723117+4JNxDybbuk@users.noreply.github.com>
2024-10-25 22:03:03 +0530

    update scripts

--- test.py
+++ test.py
@@ -1,3 +1,8 @@
 print("ANIKET & NITU will be married at DEC, 2026 ")
 print("NITU will be completed her MBA at 2027")
-print(27 + 11)
+
+a = 27
+b = 11
+print(a+b)
+
+print("End of file................")
