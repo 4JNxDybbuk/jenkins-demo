34964bc70566cbeb8071e4a3ae12f285975d272a
aniket.dutta1998 <aniket.dutta@crystaltechlab.com>
2024-11-13 09:23:42 +0530

    main:add user list & create node server

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-console.log("Implements CI/CD using Jenkins pipeline....")
+const http = require('http');
+
+// Define server port
+const PORT = 5050;
+const userDb = require('./user.constant');
+
+// Create the HTTP server
+const server = http.createServer((req, res) => {
+    // Set the response header for JSON content
+    res.setHeader('Content-Type', 'application/json');
+
+    if (req.method === 'GET' && req.url === '/') {
+        // Example API endpoint /api/hello
+        res.writeHead(200);
+        res.end(JSON.stringify({ message: 'Server is running at 5050' }));
+    }
+    // Define a simple router
+    if (req.method === 'GET' && req.url === '/api/users') {
+        // Example API endpoint /api/hello
+        res.writeHead(200);
+        res.end(JSON.stringify({
+            message: 'Fetching user data...',
+            data: userDb
+        }));
+    }
+});
+
+// Start the server
+server.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
---
+++ user.constant.js
@@ -0,0 +1,14 @@
+module.exports = [
+    {
+        "id": 1,
+        "name": "Aniket"
+    },
+    {
+        "id": 2,
+        "name": "Nitu"
+    },
+    {
+        "id": 3,
+        "name": "Akash"
+    }
+]
